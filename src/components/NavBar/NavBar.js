0eb86a0a218f1a3f219f660b396447e6a8ca3ae9
prashant-freelance <prashant.work.1984@gmail.com>
2020-01-25 16:33:12 +0530

    navbar done

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./navbar.scss";
+import logo from "../../assets/logo.png";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar">
+      <img src={logo} alt="logo" />
+      <ul className="nav-links">
+        <li>
+          <a href="/" className="nav-link">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="/" className="nav-link active">
+            Tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
